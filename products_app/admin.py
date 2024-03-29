3cd8dd420018707677076b16db5c185e725b8a98
akbarmuminjanov <muminjanov.75@gmail.com>
2024-03-29 23:38:05 +0300

    first commit

--- products_app/admin.py
+++ products_app/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import Product, Category
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['category', 'product_id', 'name']
-    search_fields = ['product_id', 'category']
+    list_display = ['category', 'product_kod', 'name']
+    search_fields = ['product_kod', 'category']
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
--- products_app/models.py
+++ products_app/models.py
@@ -3,18 +3,32 @@ from .utils import create_new_id
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    product_id = models.CharField(max_length= 10, editable=False, unique=True, default=create_new_id)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    product_kod = models.CharField(max_length= 10, editable=False, unique=True, default=create_new_id)
     name = models.CharField(max_length=255)
-    descriptions = models.TextField(null=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to="product_image")
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=14, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to="product_image")
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
 
     def __str__(self):
         return self.name
